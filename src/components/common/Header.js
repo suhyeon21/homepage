769b42d6164d7f3f5d9c6712c202c3569ab45600
soohyun <suhyeon.kan@gmail.com>
2022-10-27 17:54:02 +0900

    Feat: header에 라우터 링크 연결

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,5 +1,48 @@
+import { Link, NavLink } from 'react-router-dom';
+
 function Header() {
-	return <div>Header</div>;
+	const active = { color: 'aqua' };
+	return (
+		<header>
+			<h1>
+				<Link to='/'>LOGO</Link>
+			</h1>
+			<nav>
+				<ul id='gnb'>
+					<li>
+						<NavLink to='/department' activeStyle={active}>
+							DEPARTMEN
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/community' activeStyle={active}>
+							COMMUNITY
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/gallery' activeStyle={active}>
+							GALLERY
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/youtube' activeStyle={active}>
+							YOUTUBE
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/location' activeStyle={active}>
+							LOCATION
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/department' activeStyle={active}>
+							MEMBERS
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
 
 export default Header;

c8373b7adf59260ad9b116828a4c700a1a9aa037
soohyun <suhyeon.kan@gmail.com>
2022-11-21 16:20:52 +0900

    Feat: Community 컴포넌트 스타일 완성

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -2,6 +2,8 @@ import Layout from '../common/Layout';
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmarkSquare } from '@fortawesome/free-solid-svg-icons';
+import { faSquareCheck } from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useRef, useState } from 'react';
 
 function Community() {
@@ -12,7 +14,10 @@ function Community() {
 
 	const inputTitle = useRef(null);
 	const inputContent = useRef(null);
+	const titleEdit = useRef(null);
+	const contentEdit = useRef(null);
 	const [Posts, setPosts] = useState([]);
+	const [Edit, setEdit] = useState(true);
 
 	const resetForm = () => {
 		inputTitle.current.value = '';
@@ -31,11 +36,53 @@ function Community() {
 			{ title: inputTitle.current.value, content: inputContent.current.value },
 			...Posts,
 		]);
+
+		resetForm();
 	};
 
 	const deletePost = (i) => {
 		const newPosts = Posts.filter((_, index) => i !== index);
 		setPosts(newPosts);
+	};
+
+	const editPost = (i) => {
+		if (!Edit) return;
+		setEdit(false);
+
+		setPosts(
+			Posts.map((post, index) => {
+				if (i === index) post.edit = true;
+				return post;
+			})
+		);
+	};
+
+	const updatePost = (i) => {
+		if (!titleEdit.current.value.trim() || !contentEdit.current.value.trim()) {
+			resetForm();
+			return alert('수정할 제목과 본문을 모두 입력해주세요.');
+		}
+
+		Posts.map((post, index) => {
+			if (i === index) {
+				post.title = titleEdit.current.value;
+				post.content = contentEdit.current.value;
+				post.edit = false;
+			}
+			return post;
+		});
+		setEdit(true);
+	};
+
+	const cancelPost = (i) => {
+		setEdit(true);
+
+		setPosts(
+			Posts.map((post, index) => {
+				if (i === index) post.edit = false;
+				return post;
+			})
+		);
 	};
 
 	return (
@@ -78,22 +125,62 @@ function Community() {
 				<ul className='board-list'>
 					{Posts.map((post, i) => {
 						return (
-							<li>
-								<h2>{post.title}</h2>
-								<p>{post.content}</p>
+							<li className='board-item' key={i}>
+								{post.edit ? (
+									<div className='input-edit-list'>
+										<input
+											type='text'
+											placeholder='title'
+											name='Title'
+											ref={titleEdit}
+											defaultValue={post.title}
+										/>
+										<textarea
+											name='content'
+											placeholder='Content'
+											ref={contentEdit}
+											defaultValue={post.content}></textarea>
 
-								<div className='btns-community-ouput'>
-									<button type='button'>
-										<FontAwesomeIcon icon={faPenToSquare}></FontAwesomeIcon>
-									</button>
-									<button
-										type='button'
-										onClick={() => {
-											deletePost(i);
-										}}>
-										<FontAwesomeIcon icon={faTrashCan}></FontAwesomeIcon>
-									</button>
-								</div>
+										<div className='btns-edit'>
+											<button
+												type='button'
+												onClick={() => {
+													cancelPost(i);
+												}}>
+												<FontAwesomeIcon icon={faXmarkSquare}></FontAwesomeIcon>
+											</button>
+											<button
+												type='button'
+												onClick={() => {
+													updatePost(i);
+												}}>
+												<FontAwesomeIcon icon={faSquareCheck}></FontAwesomeIcon>
+											</button>
+										</div>
+									</div>
+								) : (
+									<>
+										<h2>{post.title}</h2>
+										<p>{post.content}</p>
+
+										<div className='btns-community-ouput'>
+											<button
+												type='button'
+												onClick={() => {
+													editPost(i);
+												}}>
+												<FontAwesomeIcon icon={faPenToSquare}></FontAwesomeIcon>
+											</button>
+											<button
+												type='button'
+												onClick={() => {
+													deletePost(i);
+												}}>
+												<FontAwesomeIcon icon={faTrashCan}></FontAwesomeIcon>
+											</button>
+										</div>
+									</>
+								)}
 							</li>
 						);
 					})}
